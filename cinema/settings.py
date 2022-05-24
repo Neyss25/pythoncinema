99592b8b378035e49e492fbe5abc7956021cec36
Neis Marin <nmelissa09@gmail.com>
2022-05-24 00:40:08 -0500

    feat: add requirements

--- cinema/settings.py
+++ cinema/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-58**8=6vu_=q#9q6z#lo)53@ecnp3em@jy&3eo_bebux!*f3kh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = "https://pythoncinema.herokuapp.com/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
